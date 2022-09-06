4586b1a98125c752d4c04c31bc247aa7c1e4b85f
wl9322 <wl9322@gmail.com>
2022-09-06 12:56:26 +0900

    My Portfolio Page

---
+++ js/overlayload.js
@@ -0,0 +1,108 @@
+	// Reflect scrolling in navigation
+	var navActive = function(section) {
+
+		var $el = $('#navbar > ul');
+		$el.find('li').removeClass('active');
+		$el.each(function(){
+			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+		});
+
+	};
+
+	var navigationSection = function() {
+
+		var $section = $('section[data-section]');
+		
+		$section.waypoint(function(direction) {
+		  	
+		  	if (direction === 'down') {
+		    	navActive($(this.element).data('section'));
+		  	}
+		}, {
+	  		offset: '150px'
+		});
+
+		$section.waypoint(function(direction) {
+		  	if (direction === 'up') {
+		    	navActive($(this.element).data('section'));
+		  	}
+		}, {
+		  	offset: function() { return -$(this.element).height() + 155; }
+		});
+
+	};
+
+var contentWayPoint = function() {
+	var i = 0;
+	$('.animate-box').waypoint( function( direction ) {
+
+		if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+			
+			i++;
+
+			$(this.element).addClass('item-animate');
+			setTimeout(function(){
+
+				$('body .animate-box.item-animate').each(function(k){
+					var el = $(this);
+					setTimeout( function () {
+						var effect = el.data('animate-effect');
+						if ( effect === 'fadeIn') {
+							el.addClass('fadeIn animated');
+						} else if ( effect === 'fadeInLeft') {
+							el.addClass('fadeInLeft animated');
+						} else if ( effect === 'fadeInRight') {
+							el.addClass('fadeInRight animated');
+						} else {
+							el.addClass('fadeInUp animated');
+						}
+
+						el.removeClass('item-animate');
+					},  k * 200, 'easeInOutExpo' );
+				});
+				
+			}, 100);
+			
+		}
+
+	} , { offset: '85%' } );
+};
+
+function myload(url) {	
+	$(".project-overlay").load(url, contentWayPoint);
+	$(".project-overlay").removeClass("hidden");
+	$("#colorlib-page").addClass("hidden");
+}
+
+
+function closeOverlay() {
+	let curURL = $(window.location)[0].href;
+	let lastIndex = curURL.lastIndexOf("#");
+	let url = curURL.substring(0, lastIndex);
+
+	window.history.pushState(null, null, url);
+	$(".project-overlay").addClass("hidden");
+	$(".project-overlay").empty();
+	$("#colorlib-page").removeClass("hidden");
+	
+	let section = 'project';
+	navigationSection();
+	$('html, body').animate({
+		scrollTop: $('[data-section="' + section + '"]').offset().top - 55
+	}, 10);
+}
+
+$("a[name='overlay-project']").click(function(event) {
+	event.preventDefault();
+	let url = $(this).attr("path");
+	let curURL = $(window.location)[0].href;
+	let lastIndex = curURL.lastIndexOf("/");
+
+	myload(curURL.substring(0, lastIndex + 1) + url);
+
+	let loadURL = curURL.substring(0, lastIndex + 1) + "#" + url;
+	window.history.pushState(null, null, loadURL);
+	$('html, body').animate({
+		scrollTop: 0
+	}, 10);
+})
